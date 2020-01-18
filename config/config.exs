e35a9dd4acab9c0312923eeed0257cea0d152167
Ismael Ladeira Guimarães <ismaellg@gmail.com>
2020-01-18 00:50:47 -0300

    Setup mox for testing

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,5 @@
 use Mix.Config
 
 config :logger, :console, metadata: :all
+
+import_config "#{Mix.env()}.exs"
--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Pepe.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env())
     ]
   end
 
@@ -23,9 +24,13 @@ defmodule Pepe.MixProject do
   defp deps do
     [
       {:plug_cowboy, "~> 2.0"},
-      {:jason, "~> 1.1"}
+      {:jason, "~> 1.1"},
+      {:mox, "~> 0.5", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
+
+  defp elixirc_paths(:test), do: ["test/support", "lib"]
+  defp elixirc_paths(_), do: ["lib"]
 end
