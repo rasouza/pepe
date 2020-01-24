697015f70afc2f56e0d8b4119a8e31846bfb45b8
Ismael Ladeira Guimarães <ismael.guimaraes@sumup.com>
2020-01-24 16:56:13 -0300

    Removes Phoenix (#2)
    * Removes Phoenix
    
    This effectively reverts the initial two commits:
    aeee43a251e3ab57c3e992ebbf55636271f5d9e4
    97baae00b06f916744448ff804465bad2178a9aa
    
    * Reinitializes the project
    
    This adds the original files generated by `mix new .`

--- lib/pepe.ex
+++ lib/pepe.ex
@@ -1,9 +1,18 @@
 defmodule Pepe do
   @moduledoc """
-  Pepe keeps the contexts that define your domain
-  and business logic.
-
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
+  Documentation for Pepe.
   """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pepe.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
 end
